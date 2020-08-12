28b5f241c7ab989ac3901608d893d3b7e415d461
Jesse Gansler <jessegansler@gmail.com>
2020-08-12 12:39:26 -0700

    added mutation resolvers for login, booking trips, and cancelling trips

--- server/src/index.js
+++ server/src/index.js
@@ -6,11 +6,23 @@ const resolvers = require('./resolvers');
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
 
+const isEmail = require('isemail');
+
 require('dotenv').config();
 
 //create db and server
 const store = createStore();
 const server = new ApolloServer({
+    context: async ({ req }) => {
+        // simple auth check on every request
+        const auth = req.headers && req.headers.authorization || '';
+        const email = Buffer.from(auth, 'base64').toString('ascii');
+        if (!isEmail.validate(email)) return { user: null };
+        // find a user by their email
+        const users = await store.users.findOrCreate({ where: { email } });
+        const user = users && users[0] || null;
+        return { user: { ...user.dataValues } };
+      },
   typeDefs,
   resolvers,
   dataSources: () => ({
@@ -22,3 +34,8 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
+
+/*
+https://www.apollographql.com/docs/apollo-server/data/data-sources/#using-memcached-redis-as-a-cache-storage-backend
+https://www.apollographql.com/blog/easy-and-performant-graphql-over-rest-e02796993b2b/
+*/
